dca8e3b8ead43b164a6966a9b4822125a7c874d4
cordovabarbara <aliendocordova@gmail.com>
2025-09-10 15:33:11 -0500

    configure pytest.ini: add smoke marker

--- tests/test_demo1.py
+++ tests/test_demo1.py
@@ -1,8 +1,14 @@
 #Any pytest file should start with test_
 #pytest method names should start with test
+# skip test with @pytest.mark.skip
+import pytest
 
+
+@pytest.mark.smoke
+@pytest.mark.skip
 def test_firstProgram():
     print("Hello World")
 
+@pytest.mark.xfail
 def test_greting():
     print("Good Morning")
