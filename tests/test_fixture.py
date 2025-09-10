63b29e245a88963b46b4e830316fcf3707357867
cordovabarbara <aliendocordova@gmail.com>
2025-09-10 16:56:49 -0500

    Add: Pytest fixtures learning - setup/teardown with yield example

---
+++ tests/test_fixture.py
@@ -0,0 +1,10 @@
+import pytest
+
+@pytest.fixture()
+def setup():
+    print("I will executing first")
+    yield
+    print("I will executing last")
+
+def test_fixtureDemo(setup):
+    print("I will execute  steps in fixtureDemo method")
