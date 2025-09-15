23b6c5e0ba0d017673daf5ce7b8d8b13445a2ece
cordovabarbara <aliendocordova@gmail.com>
2025-09-15 09:58:47 -0500

    refactor: clean up add_product_to_cart loop and add explicit error when product is missing

--- pageObjects/shop.py
+++ pageObjects/shop.py
@@ -1,39 +1,38 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from pageObjects.checkout_confirmation import CheckoutConfirmation
 
 
 class ShopPage:
     shop_link = (By.CSS_SELECTOR, "a[href*='shop']")
     products_cards = (By.XPATH, "//div[@class='card h-100']")
-    cart_button = (By.CSS_SELECTOR, "a[class*='btn-primary']")   # arriba a la derecha
+    cart_button = (By.CSS_SELECTOR, "a.nav-link.btn.btn-primary")   # up - right
 
     def __init__(self, driver):
         self.driver = driver
-        self.wait = WebDriverWait(driver, 10)
+        self.wait = WebDriverWait(driver, 15)
 
     def open_shop(self):
         self.driver.find_element(*self.shop_link).click()
 
     def add_product_to_cart(self, product_name):
-        products = self.wait.until(
-            EC.presence_of_all_elements_located(self.products_cards)
-        )
+        products = self.driver.find_elements(*self.products_cards)
 
         for product in products:
-            productName = product.find_element(By.XPATH, ".//h4/a").text.strip()
-            if productName.lower() == product_name.lower():
-                add_button = product.find_element(By.CSS_SELECTOR, "div.card-footer button.btn.btn-info")
-                add_button.click()
+            name = product.find_element(By.XPATH, ".//h4/a").text
+            if name.strip() == product_name:
+                product.find_element(By.XPATH, ".//button").click()
+                print(f"✅ Product added to cart: {product_name}")
                 break
+        else:
+            raise Exception(f"❌ Product '{product_name}' no found.")
 
-
-        cart_text = self.wait.until(
-            EC.presence_of_element_located(self.cart_button)
-        ).text
-        print("Cart button text:", cart_text)
+        # wait until cart update
+        self.wait.until(
+            EC.text_to_be_present_in_element(self.cart_button, "Checkout ( 1 )")
+        )
 
     def go_to_cart(self):
-        self.driver.find_element(*self.cart_button).click()
-
-
+        self.wait.until(EC.element_to_be_clickable(self.cart_button)).click()
+        return CheckoutConfirmation(self.driver)
